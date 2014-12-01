30bf2d481f315fd279ee14eb62760d99c6dd42fd
SondaSengupta <sondasengupta@gmail.com>
2014-12-01 15:01:59 -0600

    all cells toggle between x, o, and unoccupied

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 ;(function(){
   'use strict';
-
+var count = 0;
+$( "td" ).each(function() {
+  var $thisCell = $(this);
+  var count = 0;
+  $thisCell.click(function() {
+    $thisCell.removeClass("unoccupied xselected oselected");
+    count++;
+    $thisCell.toggleClass( "xselected", count % 2 === 0 );
+    $thisCell.toggleClass( "oselected", count % 3 === 0 );
+  });
+});
 
 
 }())
