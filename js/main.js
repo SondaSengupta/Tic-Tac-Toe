d0bc14df6c18310f7956dc135b14d4fc5c6052b7
SondaSengupta <sondasengupta@gmail.com>
2014-12-02 20:51:31 -0600

    Add game elements and firebase recipe

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 ;(function(){
   'use strict';
-  var matrix = [[0,0,0,], [0,0,0],[0,0,0]];
+  var matrix = [["","","",],["","",""],["","",""]];
+  var turnCounter = 0;
 
   // ----- CREATE THE BOARD ----//
   matrix.forEach(creatingRows);
@@ -14,30 +15,70 @@
     });
   }
 
-  // --TOGGLE THROUGH X AND O SELECTIONS --//
-  var count = 0;
-  $("td").each(function() {
-    var $thisCell = $(this);
-    var count = 0;
-    $thisCell.click(function() {
-      count++;
-      $thisCell.removeClass("unoccupied xselected oselected");
-      $thisCell.toggleClass( "xselected", count % 2 === 0 );
-      $thisCell.toggleClass( "oselected", count % 3 === 0 );
-      if(count >= 3){
-        count = 0;
-      }
-      if ( $($thisCell).hasClass("xselected") ) {
-        alert('it is an x!');
-      };
-    });
+  // --Add Symbols to DOM and Update Matrix--//
+
+  $('td').click(function(){
+    var vertical = Math.floor($("td").index(this)/3);
+    var horizontal = $(this).index();
+    if (turnCounter % 2 === 0){
+      var x = $(this).text("X");
+      matrix[vertical][horizontal] = "X";
+    } else{
+      $(this).text("O");
+      matrix[vertical][horizontal] = "O";
+    }
+    console.log(matrix)
+    turnCounter++;
+    checkHorizontal(matrix);
+    checkVertical(matrix);
+    checkDiagonal1(matrix);
+    checkDiagonal2(matrix)
   });
 
-  // --TURN COUNTER--//
-  var turnCounter = 0;
-  $('#set').click(function(){
-    turnCounter++;
-    return turnCounter;
-  });
+  function checkHorizontal(matrix) {
+    for(var i = 0; i < 3; i++){
+      if(matrix[i][0] === matrix[i][1] && matrix[i][0] === matrix[i][2]) {
+        if(matrix[i][0] === "X"){
+          alert("Player X wins!");
+        } else if (matrix[i][0] === "O") {
+          alert("Player O wins!");
+        }
+      }
+    }
+  }
+
+  function checkVertical(matrix) {
+    for(var i = 0; i < 3; i++){
+      if(matrix[0][i] === matrix[1][i] && matrix[0][i] === matrix[2][i]) {
+        if(matrix[0][i] === "X"){
+          alert("Player X wins!");
+        } else if (matrix[0][i] === "O") {
+          alert("Player O wins!");
+        }
+      }
+    }
+  }
+
+  function checkDiagonal1(matrix){
+    if(matrix[0][0] === matrix[1][1] && matrix[0][0] === matrix[2][2]) {
+      if(matrix[0][0] === "X"){
+        alert("Player X wins!");
+      } else if (matrix[0][0] === "O") {
+        alert("Player O wins!");
+      }
+    }
+  }
+
+  function checkDiagonal2(matrix){
+    if(matrix[0][2] === matrix[1][1] && matrix[0][2] === matrix[2][0]) {
+      if(matrix[0][2] === "X"){
+        alert("Player X wins!");
+      } else if (matrix[0][2] === "O") {
+        alert("Player O wins!");
+      }
+    }
+  }
+
+
 
 }())
